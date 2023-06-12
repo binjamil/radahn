1951fd474afb7d6a3789afdf55c2292d2eba8a5e
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-06-12 16:12:58 +0500

    Add SET option for keys to expire

--- handler.cc
+++ handler.cc
@@ -1,14 +1,15 @@
 #include <cstdio>
 #include <cstring>
+#include <ctime>
 #include <map>
 #include <string>
 
-#include "protocol.hh"
 #include "handler.hh"
+#include "protocol.hh"
 
 typedef struct RadahnObject {
   char *val;
-  int exp;
+  time_t exp;
 } RadahnObject;
 
 static std::map<std::string, RadahnObject> keyspace;
@@ -61,6 +62,15 @@ static void get(Cmd *cmd, char *resp, int resp_len) {
       return;
     }
     RadahnObject obj = it->second;
+    if (obj.exp != -1) {
+      time_t now = time(NULL);
+      if (now >= obj.exp) {
+        free(obj.val);
+        keyspace.erase(it);
+        sprintf(resp, "$-1\r\n");
+        return;
+      }
+    }
     sprintf(resp, "$%lu\r\n%s\r\n", strlen(obj.val), obj.val);
   }
 }
@@ -69,13 +79,26 @@ static void set(Cmd *cmd, char *resp, int resp_len) {
   if (cmd->argc < 3) {
     sprintf(resp, "-ERR: Not enough arguments\r\n");
   } else {
+    time_t exp = -1;
+    if (cmd->argc == 5) {
+      if (strcmp(cmd->argv[3], "EX") != 0) {
+        sprintf(resp, "-ERR: Unsupported command option\r\n");
+        return;
+      };
+      int secs = atoi(cmd->argv[4]);
+      if (secs <= 0) {
+        sprintf(resp, "-ERR: Invalid EX seconds value\r\n");
+        return;
+      }
+      exp = time(NULL) + secs;
+    }
     std::string key = cmd->argv[1];
     RadahnObject obj;
     size_t val_len = strlen(cmd->argv[2]) + 1;
-    char* val = (char *)calloc(val_len, sizeof(char));
+    char *val = (char *)calloc(val_len, sizeof(char));
     memcpy(val, cmd->argv[2], val_len);
     obj.val = val;
-    obj.exp = -1;
+    obj.exp = exp;
     keyspace.insert_or_assign(key, obj);
     sprintf(resp, "+OK\r\n");
   }
